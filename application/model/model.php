22b5b157f6577a17aa33ecc879749e4b1b907ebc
csxz1122 <xsun17@cmc.edu>
2017-04-18 20:19:05 -0700

    model

--- application/model/model.php
+++ application/model/model.php
@@ -1,6 +1,6 @@
-have the getCategort --> use querie
+have the getCategory --> use querie
 
-all methods for queries
+all methods for queries:
 addItem
 getServiceItem
 
@@ -8,3 +8,96 @@ getServiceItem
 can also save session save database info here
 -->step before checkout
 -->add the item to the cart
+
+
+<?php
+	include('dbconn.php');
+?>
+
+<!DOCTYPE html>
+	<head>
+		<title>Creating a drop down</title>
+	</head>
+
+	<body>
+		<pre> <?php print_r($_GET);?> </pre>
+
+		<?php
+
+		if ( isset( $_GET['submitted'] ) )
+			handle_form( $_GET['serviceitems'] );
+		
+		display_form( "serviceitems" );
+
+		?>
+	</body>
+</html>
+
+
+
+<?php
+//add_item
+//add_customer
+//add_provider
+//add_requestOrder
+//get_category
+//send_category
+
+function handle_form( $id ){
+	echo "**********  ************** <br><br>";
+	$dbc    = connect_to_db( "imdb" );	
+	$query  = "SELECT * FROM Movie WHERE movieid = $id ";
+	$result = perform_query( $dbc, $query );
+
+	#$row = mysqli_fetch_array( $result, MYSQLI_ASSOC );
+	
+	#echo "********** <br>";
+	#print_r($row);
+	#echo "********** <br>";
+	echo "<ul>\n";
+	while ( $row = mysqli_fetch_array( $result, MYSQLI_ASSOC ) ) {
+			
+		$title = $row['title'];
+		$year  = $row['year'];
+		$director  = $row['director'];
+			   
+		echo "<li>Title: $title, Year: $year, Director: $director </li>\n";
+	}
+	echo "</ul>\n";
+	
+	disconnect_from_db( $dbc, $result );
+}
+
+function send_category( $id ){
+	
+}
+
+
+
+function display_category( $menuname ){
+	echo '<form method = "get">';
+	create_select( $menuname );
+	echo '<input type="submit" name="submitted" value="Go!"></form>';
+}
+
+function get_category( $menuname ){
+	echo "<select name= '$menuname'>\n";
+	$dbc    = connect_to_db( "serviceitems" );	
+	$query  = "SELECT name, id FROM CATEGORY";
+	$result = perform_query( $dbc, $query );
+	
+	while ($row = mysqli_fetch_array( $result, MYSQLI_ASSOC )){
+	
+		$categoryname = $row['name'];
+		$cateogryid  = $row['id'];
+	   
+	   	if ( isset( $_GET[$menuname] ) && ( $_GET[$menuname]==$cateogryid ) )
+			echo "<option value = '$cateogryid' selected> $categoryname  </option>\n";
+		else
+			echo "<option value = '$cateogryid'> $categoryname  </option>\n";
+	}
+		
+	echo "</select>";
+	disconnect_from_db( $dbc, $result );
+}
+
