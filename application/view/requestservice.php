9bf1b740cf33cf4a15ef63387286bc21f7b3a24e
csxz1122 <xsun17@cmc.edu>
2017-05-04 13:34:09 -0700

    linked request service and email function

--- application/view/requestservice.php
+++ application/view/requestservice.php
@@ -5,7 +5,37 @@
 
 
 <html lang="en">
+<style>
+input[type=text], select, textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
+}
 
+input[type=submit] {
+    background-color: #4CAF50;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+.container {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+</style>
 <head>
 <title>Provide your information:</title>
 </head>
@@ -17,11 +47,16 @@
         font-size:14pt;
     }
 </style>
-
+<script>
+function sendEmail() {
+    window.open('mailto:test@example.com');
+}
+</script>
 <body>
-
+<div class="container">
 <form name="checkout" method="post">
     <script src="public/js/validate.js"></script>
+    <h1>Book Service</h1>
     <h2>Customer Info</h2>        
         <legend for="name">Name
         <input type="text" name="name" value="" onBlur="validateField(this)" id="name"><span class='info'></span></legend>
@@ -37,11 +72,11 @@
         <h3>Note to provider:</h3>
         <legend for="note">
         <input type="text" name="note" id="note" value="" <span class='info'></span></legend>
-        <p><input type="button" value="Submit request" onBlur="sendEmail(get_ProviderEmail(), #note, #name, #school, #phone, #email)"/><span class='info'></span></p>
+        <p><input type="button" value="Submit request" onBlur="sendEmail()"/><span class='info'></span></p>
         <p><input type="reset"></p>
   
         
-    </form></fieldset>
+    </form></div>
 
 
 <p><a href="application/view/index.php">Look around some more</a></p>
